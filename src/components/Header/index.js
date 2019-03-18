cfa84540949ef36e0b07fb091c9dacf277e874c3
thais <thais04dias@gmail.com>
2019-03-18 11:58:14 -0300

    vdc

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,3 +1,15 @@
-import Header from "./Header";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import registerServiceWorker from './registerServiceWorker';
 
-export default Header;
+ReactDOM.render(
+  <App texto="Olá mundo!" />,
+  document.querySelector("root");
+);
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+serviceWorker.unregister();
