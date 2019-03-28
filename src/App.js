e7c68a018458c8e8ad9002c4b0377cb18d5a1a0f
thaisinfo <thais04dias@gmail.com>
2019-03-28 10:01:33 -0300

    componentes complexos

--- src/App.js
+++ src/App.js
@@ -1,16 +1,24 @@
 import React, { Component } from "react";
-import logo from "./logo.svg";
+// import logo from './logo.svg';
 
 import "./App.css";
 import Header from "./components/Header";
 import NewUser from "./components/NewUser";
+import Toast from "./components/Toast";
 
 class App extends Component {
   render() {
     return (
       <div>
         <Header />
-        <NewUser />
+        <NewUser
+          onSubmit={user => {
+            let gender = user.gender === "m" ? "o" : "a";
+            this.refs.toast.success(`Seja bem vind${gender} ${user.name}!`);
+          }}
+          error={msg => this.refs.toast.error(msg)}
+        />
+        <Toast ref="toast" />
       </div>
     );
   }
